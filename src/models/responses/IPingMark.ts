694b36a61de00363d8aa26f072a7e5013e25a5ca
Harlan Sang <harlansang@gmail.com>
2021-02-07 21:39:01 -0600

    fix: Minor parameter format changes

--- src/models/responses/IPingMark.ts
+++ src/models/responses/IPingMark.ts
@@ -1,28 +1,3 @@
-/**
- * @swagger
- * components:
- *   schemas:
- *     IPingMark:
- *       type: object
- *       properties:
- *         mark:
- *           type: integer
- *           format: int64
- *           example: 1591227886882
- *         application:
- *           type: string
- *           example: node-user-service
- *         version:
- *           type: string
- *           nullable: true
- *           example: 0.9.7
- *     PingMarkResponse:
- *       properties:
- *         status:
- *           $ref: '#/components/schemas/Response/properties/status'
- *         data:
- *           $ref: '#/components/schemas/IPingMark'
- */
 export interface IPingMark {
   mark: number;
   application?: string;
